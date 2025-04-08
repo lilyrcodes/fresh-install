2073e6fc8f42d94d76800b8aa0fc1353b7501e28
Lily Richards <lilymichellerichards@gmail.com>
2025-04-08 10:08:55 -0400

    Update neovim filename

--- neovim.sh
+++ neovim.sh
@@ -4,8 +4,8 @@
 set -e
 
 echo "Downloading and installing neovim..." 1>&2
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
